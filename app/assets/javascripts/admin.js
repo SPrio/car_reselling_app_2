1344b0c48d6567992cec9e532fafd538f4141880
SPrio <mahantasupriyo@gmail.com>
2019-10-21 19:37:07 +0530

    updated application.css

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,24 +1,3 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 //You can use CoffeeScript in this file: http://coffeescript.org/
-
-$('#datetimepicker1').datetimepicker({
-  format: "YYYY.MM.DD hh:mm A",
-  stepping: 15,
-  useCurrent: false,
-  sideBySide: true,
-  icons: {
-    up: 'fas fa-arrow-up',
-    down: 'fas fa-arrow-down',
-    previous: 'fas fa-chevron-left',
-    next: 'fas fa-chevron-right',
-    close: 'fas fa-times'
-  },
-  buttons: {showClose: true }
-});
-
-$('#datetimepicker').datetimepicker({
-  format:'d.m.Y H:i',
-  inline:true,
-  lang:'ru'
-});
